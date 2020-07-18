fbfa777a9788bfaf72fba24a8c4aee9542ca88de
jgarcia <jesusdgarcia@gmail.com>
2020-07-18 14:16:10 -0400

    remove window decoration

--- gui/src/MainWindow.cpp
+++ gui/src/MainWindow.cpp
@@ -38,7 +38,7 @@ void MakeWindow() {
 
   window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 
-//  gtk_window_set_decorated(GTK_WINDOW(window), FALSE);
+  gtk_window_set_decorated(GTK_WINDOW(window), FALSE);
 
   g_signal_connect(window, "delete-event", G_CALLBACK(onDelete), NULL);
 
